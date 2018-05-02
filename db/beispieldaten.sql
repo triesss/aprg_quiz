b140de2d5519c1e68915511aba7cc274684b32b0
Alex <minf101260@stud.fh-wedel.de>
2018-05-02 20:36:35 +0200

    DB modified
    Tables in plural
    game_session speichert nun die id der aktuell zu beantwortenden Frage
    Testdaten angepasst 
    
    Bsp. Select:
    select question from questions where id = (select ua_current_question from game_sessions where id = 1);
    liefert die aktuelle Frage von user a

--- db/beispieldaten.sql
+++ db/beispieldaten.sql
@@ -1,12 +1,12 @@
 -- Password = hallo123
-insert into user(email,username,password,image,age,comment,is_admin) values ('super@admin.de','superadmin','7e0e0c4012fca9f0a18c802df01e758713a0751b',NULL,42,"",1);
+insert into users (email,username,password,image,age,comment,is_admin) values ('super@admin.de','superadmin','7e0e0c4012fca9f0a18c802df01e758713a0751b',NULL,42,"",1);
 -- Password = blablabla
-insert into user(email,username,password,image,age,comment) values ('normal@user.de','normaluser','23c6834b1d353eabf976e524ed489c812ff86a7d',NULL,12,"");
-insert into question(question) values('Wieviel ist 5 + 5?');
-insert into answer (qid,answer,is_true) values(1,"10",1);
-insert into answer (qid,answer,is_true) values(1,"11",0);
-insert into answer (qid,answer,is_true) values(1,"12",0);
-insert into answer (qid,answer,is_true) values(1,"13",0);
-insert into game_session(seed,ua_id,ub_id) values ('1;2;3;4;5',1,2);
-insert into user_statistic(uid,wins) values (1,42);
-insert into user_statistic(uid) values (2);
+insert into users (email,username,password,image,age,comment) values ('normal@user.de','normaluser','23c6834b1d353eabf976e524ed489c812ff86a7d',NULL,12,"");
+insert into questions (question) values('Wieviel ist 5 + 5?');
+insert into answers (qid,answer,is_true) values(1,"10",1);
+insert into answers (qid,answer,is_true) values(1,"11",0);
+insert into answers (qid,answer,is_true) values(1,"12",0);
+insert into answers (qid,answer,is_true) values(1,"13",0);
+insert into game_sessions (seed,ua_id,ub_id,ua_current_question,ub_current_question) values (123456,1,2,1,1);
+insert into user_statistics (uid,wins) values (1,42);
+insert into user_statistics (uid) values (2);
--- db/quiz_db_queries.sql
+++ db/quiz_db_queries.sql
@@ -1,5 +1,42 @@
-CREATE TABLE user (id integer primary key autoincrement,email varchar(255) not null,username varchar(255) not null, password varchar(255) not null, image text, age int, comment varchar(255), is_admin boolean default 0);
-CREATE TABLE question (id integer primary key autoincrement, question text not null);
-CREATE TABLE answer (id integer primary key autoincrement, qid integer not null, answer text not null, is_true boolean not null, FOREIGN KEY(qid) REFERENCES question(id));
-CREATE TABLE game_session (id integer primary key autoincrement, seed varchar(255) not null, ua_id integer not null, ua_points integer default 0, ub_id integer not null, ub_points integer default 0,FOREIGN KEY(ua_id) REFERENCES user(id), FOREIGN KEY(ub_id) REFERENCES user(id));
-CREATE TABLE user_statistic (id integer primary key autoincrement, uid integer not null, wins integer default 0, loses integer default 0, draws integer default 0, FOREIGN KEY(uid) REFERENCES user(id));
+CREATE TABLE users (
+	id integer primary key autoincrement,
+	email varchar(255) not null,
+	username varchar(255) not null,
+	password varchar(255) not null,
+	image text, age int,
+	comment varchar(255),
+	is_admin boolean default 0
+);
+CREATE TABLE questions (
+	id integer primary key autoincrement,
+	question text not null
+);
+CREATE TABLE answers (
+	id integer primary key autoincrement,
+	qid integer not null,
+	answer text not null,
+	is_true boolean not null,
+	FOREIGN KEY(qid) REFERENCES questions(id)
+);
+CREATE TABLE game_sessions (
+	id integer primary key autoincrement,
+	seed integer not null,
+	ua_id integer not null,
+	ua_points integer default 0,
+	ua_current_question integer not null,
+	ub_id integer not null,
+	ub_points integer default 0,
+	ub_current_question integer not null,
+	FOREIGN KEY(ua_id) REFERENCES users(id),
+	FOREIGN KEY(ub_id) REFERENCES users(id),
+	FOREIGN KEY(ua_current_question) REFERENCES questions(id),
+	FOREIGN KEY(ub_current_question) REFERENCES questions(id)
+);
+CREATE TABLE user_statistics (
+	id integer primary key autoincrement,
+	uid integer not null,
+	wins integer default 0,
+	loses integer default 0,
+	draws integer default 0,
+	FOREIGN KEY(uid) REFERENCES users(id)
+);
